0ddfe7ea92a83d9d69d1bd6958eebdac976aae50
Kameron Pete <kameroncpete@gmail.com>
2024-05-16 19:52:03 +0100

    all functions are fully functional, everything tested on postman, back end complete

--- server/db.js
+++ server/db.js
@@ -15,7 +15,7 @@ const createTables = async () => {
         CREATE TABLE items(
             id UUID PRIMARY KEY,
             name VARCHAR(255) UNIQUE NOT NULL,
-            txt VARCHAR(1000)
+            text VARCHAR(1000)
         );
         CREATE TABLE users(
             id UUID PRIMARY KEY,
@@ -24,15 +24,15 @@ const createTables = async () => {
         );
         CREATE TABLE reviews(
             id UUID PRIMARY KEY,
-            txt VARCHAR(1000),
-            rating INTEGER CHECK (rating > 0 and rating < 6) NOT NULL,
+            text VARCHAR(1000),
+            rating INTEGER NOT NULL,
             item_id UUID REFERENCES items(id) NOT NULL,
             user_id UUID REFERENCES users(id) NOT NULL,
             CONSTRAINT unique_user_review UNIQUE(item_id, user_id)
         );
         CREATE TABLE comments(
             id UUID PRIMARY KEY,
-            txt VARCHAR(1000),
+            text VARCHAR(1000),
             review_id UUID REFERENCES reviews(id) NOT NULL,
             user_id UUID REFERENCES users(id) NOT NULL
         );
@@ -41,7 +41,217 @@ const createTables = async () => {
     return res.rows
 }
 
+const createUser = async ( { username, password } ) => {
+    const SQL = /*SQL*/ `
+        INSERT INTO users(id, username, password)
+        VALUES ($1,$2,$3)
+        RETURNING * 
+    `
+    const res = await client.query(SQL, [uuid.v4(), username, await bcrypt.hash(password, 5)])
+    return res.rows[0]
+}
+
+const createComment = async ( { text, reviewId, userId } ) => {
+    const SQL = /*SQL*/ `
+        INSERT INTO comments(id, text, review_id, user_id)
+        VALUES ($1,$2,$3,$4)
+        RETURNING * 
+    `
+    const res = await client.query(SQL, [uuid.v4(), text, reviewId, userId])
+    return res.rows[0]
+}
+
+const createItem = async ( { name, text } ) => {
+    const SQL = /*SQL*/ `
+        INSERT INTO items(id, name, text)
+        VALUES ($1,$2,$3)
+        RETURNING * 
+    `
+    const res = await client.query(SQL, [uuid.v4(), name, text])
+    return res.rows[0]
+}
+
+const createReview = async ( {text, rating, itemId, userId} ) => {
+    const SQL = /*SQL*/ `
+        INSERT INTO reviews(id, text, rating, item_id, user_id)
+        VALUES ($1,$2,$3,$4,$5)
+        RETURNING * 
+    `
+    const res = await client.query(SQL, [uuid.v4(), text, rating, itemId, userId])
+    return res.rows[0]
+}
+
+const fetchItems = async () => {
+    const SQL = /*SQL*/ `
+        SELECT * FROM items
+    `
+    const res = await client.query(SQL)
+    return res.rows
+}
+
+const fetchSingleItem = async (id) => {
+    console.log(id);
+    const SQL = /*SQL*/ `
+        SELECT * FROM items 
+        WHERE id = $1
+    `
+    const res = await client.query(SQL, [id])
+    return res.rows
+}
+
+const fetchItemReviews = async (id) => {
+    console.log(id);
+    const SQL = /*SQL*/ `
+        SELECT * FROM reviews
+        WHERE item_id = $1
+    `
+    const res = await client.query(SQL, [id])
+    return res.rows
+}
+
+const fetchSingleReview = async (reviewId) => {
+    console.log(reviewId);
+    const SQL = /*SQL*/ `
+        SELECT * FROM reviews
+        WHERE id = $1
+    `
+    const res = await client.query(SQL, [reviewId])
+    console.log(res.rows);
+    return res.rows
+}
+
+function lorem(length) {
+    let words = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".split(' ');
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += words[Math.floor(Math.random() * words.length)] + ' ';
+    }
+    return result.trim();
+  }
+
+  const authenticate = async ( { username, password } ) => {
+    const SQL = /*SQL*/ `
+        SELECT id, password 
+        FROM users
+        WHERE username = $1
+    `
+    const res = await client.query(SQL, [username])
+    if(!res.rows.length || await bcrypt.compare(password, res.rows[0].password) === false) {
+        const error = Error('Not Authorized')
+        error.status = 401
+        throw error
+    }
+    const token = await jwt.sign({id: res.rows[0].id}, JWT)
+    return { token: token}
+  }
+
+const findUserByToken = async (token) => {
+    let id
+    try {
+        const payload = await jwt.verify(token, JWT)
+        id = payload.id
+    } catch (ex) {
+        const error = Error('not authorized')
+        error.status = 401
+        throw error
+    }
+    const SQL = /*SQL*/ `
+    SELECT id, username
+    FROM users
+    WHERE id = $1
+    `
+    const res = await client.query(SQL, [id])
+    if(!res.rows.length) {
+        const error = Error('Not Authorized')
+        error.status = 401
+        throw error
+    }
+    return res.rows[0]
+}
+
+const getMyReviews = async (userId) => {
+    const SQL = /*SQL*/ `
+        SELECT * 
+        FROM reviews
+        WHERE user_id = $1
+    `
+    const res = await client.query(SQL, [userId])
+    return res.rows
+}
+
+const getMyComments = async (userId) => {
+    console.log(userId);
+    const SQL = /*SQL*/ `
+        SELECT * 
+        FROM comments
+        WHERE user_id = $1
+    `
+    const res = await client.query(SQL, [userId])
+    console.log(res.rows);
+    return res.rows
+}
+
+const editReview = async ({text, rating, reviewId}) => {
+    const SQL = /*SQL*/ `
+        UPDATE reviews
+        SET text=$1, rating=$2
+        WHERE id=$3
+        RETURNING *
+    `
+    const res = await client.query(SQL, [text, rating, reviewId])
+    console.log(res.rows);
+    return res.rows
+}
+
+const deleteReview = async({userId, reviewId}) => {
+    const SQL = /*SQL*/ `
+        DELETE FROM reviews
+        WHERE id=$1 and user_id=$2
+    `
+    await client.query(SQL, [reviewId, userId])
+}
+
+const editComment = async ({text, commentId}) => {
+    console.log(text);
+    console.log(commentId);
+    const SQL = /*SQL*/ `
+        UPDATE comments
+        SET text=$1
+        WHERE id=$2
+        RETURNING *
+    `
+    const res = await client.query(SQL, [text, commentId])
+    console.log(res.rows);
+    return res.rows
+}
+
+
+const deleteComment = async({userId, commentId}) => {
+    const SQL = /*SQL*/ `
+        DELETE FROM comments
+        WHERE id=$1 and user_id=$2
+    `
+    await client.query(SQL, [commentId, userId])
+}
+
 module.exports = {
     client,
-    createTables
+    createTables,
+    createUser,
+    createItem,
+    createComment,
+    createReview,
+    fetchItems,
+    lorem,
+    fetchSingleItem,
+    fetchItemReviews,
+    fetchSingleReview,
+    authenticate,
+    findUserByToken,
+    getMyReviews,
+    editReview,
+    deleteReview,
+    getMyComments,
+    editComment,
+    deleteComment
 }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,264 @@
 const express = require('express');
 const { client,
-        createTables
+        lorem,
+        createTables,
+        createComment,
+        createUser,
+        createItem,
+        createReview,
+        fetchItems,
+        fetchSingleItem,
+        fetchItemReviews,
+        fetchSingleReview,
+        authenticate,
+        findUserByToken,
+        getMyReviews,
+        editReview,
+        deleteReview,
+        getMyComments, 
+        editComment,
+        deleteComment
  } = require('./db');
 const app = express();
 app.use(express.json());
 app.use(require('morgan')('dev'))
 require('dotenv').config
 
+
+// Middleware for isLoggedIn
+const isLoggedIn = async(req, res, next) => {
+    try {
+        console.log(req.headers.authorization);
+        req.headers.authorization = req.headers.authorization.replace(
+            "Bearer ",
+            ""
+        )
+        req.user = await findUserByToken(req.headers.authorization)
+        console.log(req.user)
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+//   GET /api/items
+app.get("/api/items", async (req, res, next) => {
+    try {
+        res.send(await fetchItems())
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   GET /api/items/:id
+app.get("/api/items/:id", async (req, res, next) => {
+    try {
+        res.send(await fetchSingleItem(req.params.id))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   POST /api/auth/register
+app.post("/api/auth/register", async (req, res, next) => {
+    try {
+        res.status(201).send(await createUser({username: req.body.username, password: req.body.password}))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   POST /api/auth/login
+app.post("/api/auth/login", async(req,res,next) => {
+    try {
+        res.status(202).send(await authenticate({username: req.body.username, password: req.body.password}))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   GET /api/auth/me (isLoggedIn)
+  
+app.get("/api/auth/me", isLoggedIn, async(req, res, next) => {
+    try {
+        const user = req.user
+        console.log(user);
+        res.send(user)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   GET /api/items/:itemId/reviews
+app.get("/api/items/:itemId/reviews", async (req,res,next) => {
+    try {
+        res.status(200).send(await fetchItemReviews(req.params.itemId))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   GET /api/items/:itemId/reviews/:id
+app.get("/api/items/:itemId/reviews/:id", async (req,res,next) => {
+    try {
+        res.status(200).send(await fetchSingleReview(req.params.id))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   POST /api/items/:itemId/reviews (isLoggedIn)
+app.post("/api/items/:itemId/reviews", isLoggedIn, async (req, res, next) => {
+    try {
+        res.status(202).send(await createReview({
+            text: req.body.text, 
+            rating: req.body.rating, 
+            itemId: req.params.itemId, 
+            userId: req.user.id}))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+//   GET /api/reviews/me (isLoggedIn)
+app.get("/api/reviews/me", isLoggedIn, async( req, res, next) => {
+    try {
+        res.send(await getMyReviews(req.user.id))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   PUT /api/users/:userId/reviews/:id (isLoggedIn)
+app.put("/api/users/:userId/reviews/:id", isLoggedIn, async(req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+        const error = Error('Not Authorized')
+        error.status(401)
+        throw error
+    }
+    try {
+        res.status(202).send(await editReview({text: req.body.text, rating: req.body.rating, reviewId: req.params.id}))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   DELETE /api/users/:userId/reviews/:id (isLoggedIn)
+app.delete("/api/users/:userId/reviews/:id", isLoggedIn, async(req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+        const error = Error('Not Authorized')
+        error.status(401)
+        throw error
+    }
+    try {
+        await deleteReview({userId: req.params.userId, reviewId: req.params.id})
+        res.sendStatus(204)
+    } catch (error) {
+        next(error)
+    }
+}) 
+
+  
+//   POST /api/items/:itemId/reviews/:id/comments (isLoggedIn)
+
+app.post("/api/items/:itemId/reviews/:id/comments", isLoggedIn, async (req, res, next) => {
+    try {
+        res.status(202).send(await createComment(
+            {text: req.body.text, 
+             reviewId: req.params.id, 
+             userId: req.body.userId}))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//   GET /api/comments/me (isLoggedIn)
+app.get("/api/comments/me", isLoggedIn, async( req, res, next) => {
+    try {
+        res.send(await getMyComments(req.user.id))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+//   PUT /api/users/:userId/comments/:id (isLoggedIn)
+app.put("/api/users/:userId/comments/:id", isLoggedIn, async(req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+        const error = Error('Not Authorized')
+        error.status(401)
+        throw error
+    }
+    try {
+        res.status(202).send(await editComment({text: req.body.text, commentId: req.params.id}))
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+//   DELETE /api/users/:userId/comments/:id (isLoggedIn)
+app.delete("/api/users/:userId/comments/:id", isLoggedIn, async(req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+        const error = Error('Not Authorized')
+        error.status(401)
+        throw error
+    }
+    try {
+        await deleteComment({userId: req.params.userId, commentId: req.params.id})
+        res.sendStatus(204)
+    } catch (error) {
+        next(error)
+    }
+}) 
+
+
+
+
+
 const init = async () => {
     await client.connect()
     console.log("connected");
     createTables()
+    const [kam, chels, max] = await Promise.all([
+        createUser({username: "kam", password: "kam123"}),
+        createUser({username: "chels", password: "chels123"}),
+        createUser({username: "max", password: "max123"})
+    ])
+
+    const [hydroflask, mouse, airpods, coaster] = await Promise.all([
+        createItem({name:"hydroflask", text:"32oz insulated water mug"}),
+        createItem({name:"mouse", text:"wireless, black, clicks"}),
+        createItem({name:"airpods", text:"bluetooth, in a green rubber case"}),
+        createItem({name:"coaster", text:"put your drinks on it, its square"}),
+    ])
+
+    const [review1, review2, review3, review4] = await Promise.all([
+        createReview({text: lorem(50), rating: 4, itemId: hydroflask.id, userId: kam.id}),
+        createReview({text: lorem(29), rating: 3, itemId: mouse.id, userId: kam.id}),
+        createReview({text: lorem(75), rating: 5, itemId: airpods.id, userId: chels.id}),
+        createReview({text: lorem(37), rating: 1, itemId: coaster.id, userId: max.id})
+    ])
+
+    createComment({text: lorem(110), reviewId: review1.id, userId: chels.id}),
+    createComment({text: lorem(90), reviewId: review2.id, userId: max.id}),
+    createComment({text: lorem(40), reviewId: review3.id, userId: kam.id}),
+    createComment({text: lorem(15), reviewId: review4.id, userId: chels.id}),
+
     app.listen(process.env.PORT, console.log(`liseting on port ${process.env.PORT}`))
 }
 
